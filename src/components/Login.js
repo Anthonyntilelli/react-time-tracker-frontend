b02310273baa487a19203071eb0bf01b27a9aa30
Anthony Tilelli <Anthony@tilelli.me>
2020-10-24 21:18:37 -0400

    Converted to Bootstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,8 @@
 import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
@@ -10,19 +14,24 @@ const LoginForm = () => {
     alert('Placeholder action');
     event.preventDefault();
   }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email: <input type='email' name='email' value={email} onChange={handleChange_email} required/>
-      </label>
-      <br/>
-      <label>
-        Password:<input type='password' name='password' value={password} onChange={handleChange_password} required/>
-      </label>
-
-      <input type="submit" value="Submit" />
-    </form>
+    <Row>
+      <Col></Col>
+      <Col>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId='loginEmail'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type='email' placeholder='user@example.com' value={email} onChange={handleChange_email} autoFocus required/>
+          </Form.Group>
+          <Form.Group controlId='LoginPassword'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' value={password} minlength='8' maxLength='72' onChange={handleChange_password} required/>
+            </Form.Group>
+          <Button variant='primary' type='submit'> Submit </Button>
+        </Form>
+      </Col>
+      <Col></Col>
+    </Row>
   )
 }
 export default LoginForm
