09beef6361ada15ed2167addcd8b0d3421ed0eb9
Anthony Tilelli <Anthony@tilelli.me>
2020-10-25 14:35:00 -0400

    Changing from email to name

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,11 +5,9 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
+  const [name, setname] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleChange_email  = (event) => setEmail(event.target.value)
-  const handleChange_password = (event) => setPassword(event.target.value)
   const handleSubmit = (event) => {
     alert('Placeholder action');
     event.preventDefault();
@@ -19,13 +17,13 @@ const LoginForm = () => {
       <Col></Col>
       <Col>
         <Form onSubmit={handleSubmit}>
-          <Form.Group controlId='loginEmail'>
-            <Form.Label>Email</Form.Label>
-            <Form.Control type='email' placeholder='user@example.com' value={email} onChange={handleChange_email} autoFocus required/>
+          <Form.Group controlId='loginname'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type='text' placeholder='John Doe' value={name} onChange={event => setname(event.target.value)} autoFocus required/>
           </Form.Group>
           <Form.Group controlId='LoginPassword'>
             <Form.Label>Password</Form.Label>
-            <Form.Control type='password' value={password} minlength='8' maxLength='72' onChange={handleChange_password} required/>
+            <Form.Control type='password' value={password} minLength='8' maxLength='72' onChange={event => setPassword(event.target.value)} required/>
             </Form.Group>
           <Button variant='primary' type='submit'> Submit </Button>
         </Form>
