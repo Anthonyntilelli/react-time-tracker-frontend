68700c7241de882400a5b6d2463a407f667f3334
Anthony Tilelli <Anthony@tilelli.me>
2020-10-24 22:08:24 -0400

    Removed store connection to src/old + shrtened css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css';
 import HeaderAndLogo from './components/HeaderAndLogo';
 import HomePage from './components/HomePage';
 import TopAlert from './components/TopAlert';
