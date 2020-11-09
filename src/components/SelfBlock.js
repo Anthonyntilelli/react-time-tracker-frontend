8fbd6b521bb564d31308827e262c89ef6d21bf92
Anthony Tilelli <Anthony@tilelli.me>
2020-11-08 22:56:28 -0500

    Move state for self to selfblock

--- src/components/SelfBlock.js
+++ src/components/SelfBlock.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import { fetchSelf } from '../redux/SelfSlice';
 import { connect } from 'react-redux';
 
 const SelfBlock = (props) => {
   const [clockEvent, SetClockEvent] = useState('');
+  const [name, setName] = useState('Loading, please standby....');
+  const [ptoCurrent, setPtoCurrent] = useState(-1);
+  const [ptoRate, setPtoRate] = useState(-1);
+  const [ptoMax, setPtoMax] = useState(-1);
   const loggedIn = props.loggedIn;
   const token = props.token;
   const id = props.self_id;
-  const fetchSelf = props.fetchSelf;
 
   const fetchClockEvent = async (token, id) => {
     const configObj = {
@@ -48,48 +50,66 @@ const SelfBlock = (props) => {
     fetchClockEvent(token,id);
   }
 
-  useEffect(()=> {
-    if (loggedIn) {
-      const urlAndRequest = { url: `http://localhost:3001/api/employee/${id}`, token: token, };
-      fetchSelf(urlAndRequest);
+  const fetchPersonalInfo = async (token, url) => {
+    const configObj = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+    };
+    const responce = await fetch(url, configObj);
+    if (!responce.ok) { // Error
+      console.error(`Fetch Personal Error: ${responce.statusText}`);
+      // TODO: Display failure alert
+    } else {
+      const json = await responce.json();
+      console.log(json);
+      setName(json.name);
+      setPtoCurrent(json.pto_current);
+      setPtoRate(json.pto_rate);
+      setPtoMax(json.pto_max);
     }
-  }, [loggedIn, token, fetchSelf, id]
-  );
+  }
 
-  useEffect(() => { fetchClockEvent(token, id) }, [token, id]);
+  useEffect(
+    ()=> { if (loggedIn) { fetchPersonalInfo(token, `http://localhost:3001/api/employee/${id}`) } },
+    [loggedIn, token, id]
+  );
+  useEffect(
+    () => { fetchClockEvent(token, id) },
+    [token, id]
+  );
 
   return (
     <>
-      <Row><Col><p>Name: {props.name}</p></Col></Row>
-      <Row><Col><p>PTO Current: {props.pto_current}</p></Col></Row>
-      <Row><Col><p>PTO Rate: {props.pto_rate}</p></Col></Row>
-      <Row><Col><p>PTO Max: {props.pto_max}</p></Col></Row>
+      <Row><Col><p>Name: {name}</p></Col></Row>
+      <Row><Col><p>PTO Current: {ptoCurrent}</p></Col></Row>
+      <Row><Col><p>PTO Rate: {ptoRate}</p></Col></Row>
+      <Row><Col><p>PTO Max: {ptoMax}</p></Col></Row>
 
-      <Row>
-        <Col>
-          { clockEvent && <h3> Clock Event:</h3> }
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          { clockEvent && <Button variant="primary" size="lg" onClick={() => handleClockEvent(clockEvent, token, id) }> {clockEvent} </Button>  }
-        </Col>
-      </Row>
+      { clockEvent &&
+        <>
+          <Row>
+            <Col>
+              <h3> Clock Event:</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button variant="primary" size="lg" onClick={() => handleClockEvent(clockEvent, token, id) }> {clockEvent}</Button>
+            </Col>
+          </Row>
+        </>
+      }
     </>
   )
 }
 
-const mapDispatchToProps = dispatch => ({
-  fetchSelf: (endpoint) => dispatch(fetchSelf(endpoint))
-});
-const mapStateToProps = state => ({
-  loggedIn: state.user.loggedIn,
-  name: state.self.name,
-  pto_current: state.self.pto_current,
-  pto_rate: state.self.pto_rate,
-  pto_max: state.self.pto_max,
-  self_id: state.user.id,
-  token: state.user.jwt
-});
+const mapDispatchToProps = dispatch => ({});
+const mapStateToProps = state => (
+  {
+    loggedIn: state.user.loggedIn,
+    self_id: state.user.id,
+    token: state.user.jwt
+  }
+);
 
-export default connect(mapStateToProps, mapDispatchToProps)(SelfBlock);
+export default connect(mapStateToProps, null)(SelfBlock);
